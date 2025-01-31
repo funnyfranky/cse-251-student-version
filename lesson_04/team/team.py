94a18a467714284a487f445a870b73bc3c60ad34
funnyfranky <cha23071@byui.edu>
2025-01-31 14:33:44 -0700

    small changes. incomplete assignment

--- lesson_04/team/team.py
+++ lesson_04/team/team.py
@@ -23,7 +23,7 @@ import json
 # Include cse 251 common Python files
 from cse251 import *
 
-RETRIEVE_THREADS = 4        # Number of retrieve_threads
+RETRIEVE_THREADS = 1        # Number of retrieve_threads
 NO_MORE_VALUES = 'No more'  # Special value to indicate no more items in the queue
 
 def retrieve_thread():  # TODO add arguments
@@ -39,7 +39,7 @@ def retrieve_thread():  # TODO add arguments
 
 
 
-def file_reader(): # TODO add arguments
+def file_reader(log): # TODO add arguments
     """ This thread reading the data file and places the values in the data_queue """
 
     # TODO Open the data file "urls.txt" and place items into a queue
@@ -56,13 +56,17 @@ def main():
     log = Log(show_terminal=True)
 
     # TODO create queue
+    q = queue.Queue()
+    
+        
     # TODO create semaphore (if needed)
 
     # TODO create the threads. 1 filereader() and RETRIEVE_THREADS retrieve_thread()s
     # Pass any arguments to these thread need to do their job
 
-    log.start_timer()
 
+
+    log.start_timer()
     # TODO Get them going - start the retrieve_threads first, then file_reader
 
     # TODO Wait for them to finish - The order doesn't matter
