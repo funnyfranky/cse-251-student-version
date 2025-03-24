28406e65b0b802b2c928aa50863213ddb995b68c
Josh Chapman <cha23071@byui.edu>
2025-03-23 21:55:38 -0600

    topic: circular buffer completed. Maybe some bugs, noticed if I change number of readers and writers it doesn't always complete. However with 2 each it works consistantly.

--- lesson_10/prove/prove.py
+++ lesson_10/prove/prove.py
@@ -67,19 +67,41 @@ WRITERS = 2
 
 WRITE_INDEX = BUFFER_SIZE + 0
 READ_INDEX = BUFFER_SIZE + 1
-FINISHED_INDEX = BUFFER_SIZE + 2
+NEXT_NUM_INDEX = BUFFER_SIZE + 2
+FINISHED_INDEX = BUFFER_SIZE + 3
 
 def returnIncrement(val):
-    return 0 if (val + 1 >= BUFFER_SIZE) else val + 1
+    return (val + 1) % BUFFER_SIZE
 
 def writer(sharedList,empty_spaces,full_spaces,lock,id):
-    empty_spaces.acquire()
-    with lock.acquire():
-        sharedList[sharedList[WRITE_INDEX]]
+    while True:
+        empty_spaces.acquire()
+        # print(f'{sharedList}')
+        with lock:
+            if sharedList[NEXT_NUM_INDEX] > sharedList[FINISHED_INDEX]: # checks if writers are finished, ends
+                if (id == 1):
+                    sharedList[sharedList[READ_INDEX]] = None # Signal to readers to stop
+                    for i in range(WRITERS): full_spaces.release()
+                return
+            sharedList[sharedList[WRITE_INDEX]] = sharedList[NEXT_NUM_INDEX]
+            sharedList[NEXT_NUM_INDEX] += 1
+            sharedList[WRITE_INDEX] = returnIncrement(sharedList[WRITE_INDEX])
+        full_spaces.release()
 
-def reader(sharedList,empty_spaces,full_spaces,lock):
-    currentVal = sharedList[WRITE_INDEX]
-    ...
+def reader(sharedList,empty_spaces,full_spaces,lock,id):
+    while True:
+        full_spaces.acquire()
+        with lock:
+            if sharedList[sharedList[READ_INDEX]] == None:
+                print(f'Reader {id} shutting down')
+                if id == 1:
+                    for i in range(READERS): empty_spaces.release()
+                return
+            readVal = sharedList[sharedList[READ_INDEX]]
+            print(readVal, end=', ', flush=True)
+            sharedList[READ_INDEX] = returnIncrement(sharedList[READ_INDEX])
+
+        empty_spaces.release()
 
 def main():
 
@@ -90,14 +112,14 @@ def main():
     smm.start()
 
     # TODO - Create a ShareableList to be used between the processes
-    sharedList = smm.ShareableList([0]*BUFFER_SIZE + [0, 0, 0, 0, 0])
+    sharedList = smm.ShareableList([0]*BUFFER_SIZE + [0, 0, 1, items_to_send])
 
     empty_spaces = mp.Semaphore(BUFFER_SIZE)
     full_spaces = mp.Semaphore(0)
     lock = mp.Lock()
 
     writerProcesses = [mp.Process(target=writer,args=(sharedList,empty_spaces,full_spaces,lock,id)) for id in range(WRITERS)]
-    readerProcesses = [mp.Process(target=reader,args=(sharedList,empty_spaces,full_spaces,lock)) for _ in range(READERS)]
+    readerProcesses = [mp.Process(target=reader,args=(sharedList,empty_spaces,full_spaces,lock,i)) for i in range(READERS)]
 
     for i in writerProcesses:
         i.start()
